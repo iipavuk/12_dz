8ffeb76540109ba5317a65a3cfdbe6fb3be05f0f
Irina <Irina@DESKTOP-PMQK38U>
2024-12-19 14:05:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DockerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Docker!");
+        }
+    }
+}
+//p
